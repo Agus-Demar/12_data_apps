0ea882985b97284795a08d1fdaa101d5fae5c839
Agus-Demar <agus.demar@gmail.com>
2022-10-30 20:45:51 -0300

    add major indexes options

--- simple_stock_price_app/stock_price_app.py
+++ simple_stock_price_app/stock_price_app.py
@@ -6,14 +6,16 @@ from datetime import datetime, timedelta
 st.write("""
 # Simple Stock Price App
 
-Shown are the stock **closing price** and ***volume*** of Google!
+Shown are the stock **closing price** and ***volume*** of major indexes!
 
 """)
 
+#dict that maps tickers
+ticker_dict = {'S&P 500':'SPY','Nasdaq':'QQQ','Industrial Dow Jones':'DIA','Russell 2000':'IWM'}
 #define the ticker symbol
-tickerSymbol = 'SPY'
+selected_ticker = st.selectbox('Choose one of the following tickers from major indexes', ['S&P 500','Nasdaq','Industrial Dow Jones','Russell 2000'])
 #get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
+tickerData = yf.Ticker(ticker_dict[selected_ticker])
 #get the historical prices for this ticker
 tickerDF = tickerData.history(period='1d', start=datetime.today() - timedelta(days=365), end=datetime.today())
 # Open   High     Low Close    Volume    Dividends   Stock Splits
